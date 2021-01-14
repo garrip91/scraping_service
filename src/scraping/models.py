8db99beead3425827a7c038d0d7bb48b3db5049b
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-01-15 00:03:25 +0300

    Summary

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,16 +1,15 @@
 from django.db import models
+
 from .utils import from_cyrillic_to_eng
 
 # Create your models here:
 class City(models.Model):
-    name = models.CharField(max_length=50,
-                            verbose_name='Название населённого пункта',
-                            unique=True)
+    name = models.CharField(max_length=50, verbose_name="Название населённого пункта", unique=True)
     slug = models.CharField(max_length=50, blank=True, unique=True)
     
     class Meta:
-        verbose_name = 'Название населённого пункта'
-        verbose_name_plural = 'Названия населённых пунктов'
+        verbose_name = "Название населённого пункта"
+        verbose_name_plural = "Названия населённых пунктов"
         
     def __str__(self):
         return self.name
@@ -23,14 +22,12 @@ class City(models.Model):
         
         
 class Language(models.Model):
-    name = models.CharField(max_length=50,
-                            verbose_name='Язык программирования',
-                            unique=True)
+    name = models.CharField(max_length=50, verbose_name="Язык программирования", unique=True)
     slug = models.CharField(max_length=50, blank=True, unique=True)
     
     class Meta:
-        verbose_name = 'Язык программирования'
-        verbose_name_plural = 'Языки программирования'
+        verbose_name = "Язык программирования"
+        verbose_name_plural = "Языки программирования"
         
     def __str__(self):
         return self.name
@@ -44,16 +41,16 @@ class Language(models.Model):
         
 class Vacancy(models.Model):
     url = models.URLField(unique=True)
-    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
-    company = models.CharField(max_length=250, verbose_name='Компания')
-    description = models.TextField(verbose_name='Описание вакансии')
-    city = models.ForeignKey('City', models.CASCADE, verbose_name='Город')
-    language = models.ForeignKey('Language', models.CASCADE, verbose_name='Язык программирования')
+    title = models.CharField(max_length=250, verbose_name="Заголовок вакансии")
+    company = models.CharField(max_length=250, verbose_name="Компания")
+    description = models.TextField(verbose_name="Описание вакансии")
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name="Город")
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name="Язык программирования")
     timestamp = models.DateField(auto_now_add=True)
     
     class Meta:
-        verbose_name = 'Вакансия'
-        verbose_name_plural = 'Вакансии'
+        verbose_name = "Вакансия"
+        verbose_name_plural = "Вакансии"
         
     def __str__(self):
         return self.title
--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
 
 from .models import Vacancy
-from .forms import FindForm
 
 # Create your views here:
 def home_view(request):
-    #print(request.GET)
-    form = FindForm()
-    city = request.GET.get('city')
-    language = request.GET.get('language')
-    qs = []
-    _filter = {}
-    if city or language:
-        #_filter = {}
-        if city:
-            _filter['city__slug'] = city
-        if language:
-            _filter['language__slug'] = language
-    qs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
+    qs = Vacancy.objects.all()
+    return render(request, 'scraping/home.html', {'object_list': qs})
