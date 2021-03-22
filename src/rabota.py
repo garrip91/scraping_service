b16341d7c271f0de6e81b4c92687121c3c071337
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-03-23 00:12:20 +0300

    Summary

--- src/rabota.py
+++ src/rabota.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup as BS
 
 import json
 
-# 1. <div id="pjax-job-list">
-# 2. <div class="card card-hover card-visited wordwrap job-link"> -> job-link
+# 1. <table class="f-vacancylist-tablewrap" ...>
+# 2. <tr id="..."> -> <div class="card-body">
+
+# 3. <p class="card-title">
+# 4. <div class="card-description">
 
 
 
@@ -13,9 +16,9 @@ import json
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
            
-domain = 'https://www.work.ua'
-url = 'https://www.work.ua/ru/jobs-kyiv-python/'
-
+domain = 'https://www.rabota.ua'
+url = 'https://rabota.ua/zapros/python/%D0%BA%D0%B8%D0%B5%D0%B2'
+'''
 resp = requests.get(url, headers=headers)
 jobs = []
 errors = []
@@ -53,5 +56,10 @@ else:
 
 
 
-with codecs.open('work.json', 'w', 'utf-8') as file:
+with codecs.open('rabota.json', 'w', 'utf-8') as file:
     json.dump(jobs, file, ensure_ascii=False, indent=4)
+'''
+resp = requests.get(url, headers=headers)
+
+with codecs.open('rabota.html', 'w', 'utf-8') as file:
+    file.write(str(resp.text))
